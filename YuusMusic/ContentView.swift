adb7dd6854d648ec2f54085ec746e10ba5a7452f
yuukimotai <yuus.saddharma@gmail.com>
2022-11-09 15:09:00 +0900

    Initial Commit

---
+++ YuusMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YuusMusic
+//
+//  Created by 茂田井雄輝 on 2022/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YuusMusic/YuusMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuusMusicApp.swift
+//  YuusMusic
+//
+//  Created by 茂田井雄輝 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct YuusMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
